827167c0d48c2c9f71d75e41036772708de2516d
Conor <mcgoverncb@gmail.com>
2016-09-08 22:31:20 +0100

    Add functionality to user service and make docker build disable prop

--- users-service/src/main/java/services/Application.java
+++ users-service/src/main/java/services/Application.java
@@ -3,10 +3,19 @@ package services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class Application {
+
+    @RequestMapping("/getuser")
+    public String getUser() {
+        return "TestUser";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
